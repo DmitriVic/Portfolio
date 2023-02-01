52675655da14d773cdc7979a68dff56744df1216
DmitriVic <Mr_vlasov@mail.ru>
2023-02-01 22:38:27 +0300

    first commit

--- components/AboutMe2/AboutMe2.props.ts
+++
@@ -1,9 +0,0 @@
-// example
-
-import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
-
-// import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
-
-export interface AboutMe2props extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
-	
-}
