6932e17578e93e2e136013692267e090c44f5849
DmitriVic <Mr_vlasov@mail.ru>
2023-02-02 21:25:33 +0300

    test

--- temp/Dota/.props.ts
+++
@@ -1,9 +0,0 @@
-// example
-
-// import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
-
-// export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-// 	children: ReactNode;
-// 	appearance: 'primary' | 'ghost';
-// 	arrow?: 'right' | 'down' | 'none';
-// }
