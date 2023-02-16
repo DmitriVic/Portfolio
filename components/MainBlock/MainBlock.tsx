c082090976b3e2bed6d49a5e0cb03d2878919c53
DmitriVic <Mr_vlasov@mail.ru>
2023-02-16 13:54:32 +0300

    добавил баннер на сайт

--- components/MainBlock/MainBlock.tsx
+++ components/MainBlock/MainBlock.tsx
@@ -1,8 +1,10 @@
- import s from './MainBlock.module.css'
+import s from "./MainBlock.module.css";
+import cn from "classnames";
 // import ArrowIcon from './arrow.svg';
 
-import {   useEffect, useState } from "react";
-import { MainBlockProps } from './MainBlock.props';
+import { useEffect, useState } from "react";
+import { MainBlockProps } from "./MainBlock.props";
+import { TextAnim } from "text-animations-react";
 
 // export const Button = ({ appearance, arrow = 'none',  children, className, ...props }: ButtonProps): JSX.Element => {
 // 	return (
@@ -18,35 +20,53 @@ import { MainBlockProps } from './MainBlock.props';
 // 			})}>
 // 				<ArrowIcon />
 // 			</span>}
-			
+
 // 	</button>)
 // }
 
-
 export const MainBlock = ({ children }: MainBlockProps): JSX.Element => {
-	const [stateText, setStateText] = useState<string>('')
+  const [stateText, setStateText] = useState<string>("");
+  const [bannerVisible, setBannerVisible] = useState(false);
+  const [sizeT, setSizeT] = useState(3);
 
-	
-useEffect(() => {
-	const text = children;
-	let count: number = 2000
-	if (stateText.length > 0) {
-		count = 60
+
+  useEffect(() => {
+    const text = children;
+	 
+    let count: number = 2000;
+    if (stateText.length > 0) {
+      count = 60;
+    }
+    let TimeId = setTimeout(() => {
+      setStateText(stateText + text[stateText?.length]);
+    }, count);
+
+    if (stateText.length === text?.length) {
+      setBannerVisible(true);
+      clearTimeout(TimeId);
+    }
+	 if (window.screen.width < 400) {
+		setSizeT(2)
 	 }
-	 let TimeId = setTimeout(() => {
-		setStateText(stateText + text[stateText?.length])
-		}, count);
-		
-		if (stateText.length === text?.length) {
-		  clearTimeout(TimeId)	
-	  }
-	
-}, [stateText])
+	 
+  }, [stateText]);
+console.log(sizeT);
 
-	return (
-	<div  className={s.mainBlock}>
-	{stateText}
-	</div>
-	)
-}
-
+  return (
+    <div className={s.mainBlock}>
+      {stateText}
+      <div className={cn({ [s.active]: bannerVisible })}>
+        { bannerVisible &&
+          <TextAnim
+            name="Остался последний гоблин для найма, успей приобрести!"
+            size={sizeT}
+            type="popoutin"
+            color='yellow'
+            count="infinite"
+            duration={4} delay={0} grad1={""} grad2={""} grad3={""} grad4={""} layer1={""} layer2={""} layer3={""} layer4={""} top={0} right={0} bottom={0} left={0} styles={{}}
+          />
+        }
+      </div>
+    </div>
+  );
+};
